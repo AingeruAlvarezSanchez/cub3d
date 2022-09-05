7180e9b93f8c25f57b576fbe47e7e4eb3750008c
AingeruAlvarezSanchez <aingeru.alvarez@outlook.com>
2022-09-05 02:34:08 +0200

    TODO added, minilib asking for and int on a char *?

--- srcs/color_initial_errors.c
+++ srcs/color_initial_errors.c
@@ -6,7 +6,7 @@
 /*   By: aalvarez <aalvarez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 01:05:27 by aalvarez          #+#    #+#             */
-/*   Updated: 2022/09/05 02:14:29 by aalvarez         ###   ########.fr       */
+/*   Updated: 2022/09/05 02:31:39 by aalvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 #include <stdlib.h>
 
 #include <stdio.h>
+static void	ft_fill_hexa_value(t_data *data)
+{
+	printf("%X\n", data->color_r); //TODO check why minilib asks for an int but displays an hexadecimal number (char *)
+}
+
 static int	ft_check_rgb_value(t_data *data, char *line, int color_len)
 {
 	static int	color_start;
@@ -29,11 +34,17 @@ static int	ft_check_rgb_value(t_data *data, char *line, int color_len)
 		tmp = ft_substr(line, color_start, color_len);
 	if (ft_atoi(tmp) > 255 || ft_atoi(tmp) < 0)
 		return (free(tmp), 1);
-	printf("data: %d\n", data->color_b); //TODO unexpected error while creating new variables on struct
-	color_start += color_len + 1;
-	if (rgb_checker == 3)
+	if (rgb_checker == 0)
+		data->color_r = ft_atoi(tmp);
+	else if (rgb_checker == 1)
+		data->color_g = ft_atoi(tmp);
+	else if (rgb_checker == 2)
+	{
+		data->color_b = ft_atoi(tmp);
 		rgb_checker = -1;
-	return (rgb_checker++, free(tmp), 0);
+		ft_fill_hexa_value(data);
+	}
+	return (color_start += color_len + 1, rgb_checker++, free(tmp), 0);
 }
 
 static int	ft_check_color_values(t_data *data, char *line)
